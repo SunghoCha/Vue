90240254019e15c4814db19f092f08435bd301e1
SunghoCha <tjdgh3725@gmail.com>
2024-06-02 01:46:48 +0900

    3.숫자야구 - 숫자야구 기본 로직 추가

--- vue/3.숫자야구/NumberBaseball.vue
+++ vue/3.숫자야구/NumberBaseball.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <form v-on:submit="onSubmitForm">
-            <input ref="answer" maxlength="4" v-model="value" />
+        <form v-on:submit="onSubmitForm"> <!-- v-on -> @로 대체 가능, e.preventDefault() 를 submit.prevent로 미리 처리 가능 -> @submit.prevent -->
+            <input ref="answer" minlength="4" maxlength="4" v-model="value" />
             <button>입력</button>
         </form>
-        <div>시도: {{  }}</div>
+        <div>시도: {{tries.length}}</div>
+        <ul>
+            <li v-for="(t, index) in tries" :key="index">{{ t }}</li>
+        </ul>
+
     </div>
 </template>
 
@@ -13,6 +17,7 @@
     export default {
         data() {
             return {
+                tries: [],
                 value: '',
                 result: '',
             }
@@ -20,6 +25,12 @@
         methods: {
             onSubmitForm(e) {
                 e.preventDefault();
+                this.tries.push({
+                    try: this.value,
+                    result: '홈런',
+                });
+                this.value='';
+                this.$refs.answer.focus();
             }
 
         }
