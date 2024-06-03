70b36e1dc204460e7d4ec0ccea1e51157c27a242
SunghoCha <tjdgh3725@gmail.com>
2024-06-03 17:43:07 +0900

    5.가위바위보 - 초기 설정

---
+++ vue/5.가위바위보/RockScissorsPaper.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div>
+        <div id="computer" v-bind:style="{ backgroundImage: '', fontSize: '14px'}" ></div>
+        <div>
+            <button>가위</button>
+            <button>바위</button>
+            <button>보</button>
+        </div>
+        <div>{{result}}</div>
+        <div>현재 {{score}}점</div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return{
+                result: '',
+                score: 0,
+            };
+        },
+        computed: {
+            
+        },
+        methods: {
+            
+        },
+    };
+</script>
+<style lang="">
+    
+</style>
