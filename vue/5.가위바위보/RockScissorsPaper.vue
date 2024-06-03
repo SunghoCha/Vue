d66146c4624a80e6e1ec69dae8d41ce1b78a60b5
SunghoCha <tjdgh3725@gmail.com>
2024-06-03 22:32:04 +0900

    5.가위바위보 - 수정

--- vue/5.가위바위보/RockScissorsPaper.vue
+++ vue/5.가위바위보/RockScissorsPaper.vue
@@ -1,10 +1,10 @@
-<template lang="">
+<template>
     <div>
-        <div id="computer" v-bind:style="{ backgroundImage: '', fontSize: '14px'}" ></div>
+        <div id="computer" v-bind:style="{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) 0 0` }"></div>
         <div>
-            <button>가위</button>
-            <button>바위</button>
-            <button>보</button>
+            <button v-on:click="onClickButton('바위')">바위</button>
+            <button v-on:click="onClickButton('가위')">가위</button>
+            <button v-on:click="onClickButton('보')">보</button>
         </div>
         <div>{{result}}</div>
         <div>현재 {{score}}점</div>
@@ -22,7 +22,9 @@
             
         },
         methods: {
-            
+            onClickButton(choice) {
+
+            }
         },
     };
 </script>
